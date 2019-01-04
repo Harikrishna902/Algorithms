e8283a2d24d4317821e2e3435fba1f0106aada8f
Harikrishna902 <46101642+Harikrishna902@users.noreply.github.com>
2019-01-04 19:15:50 +0530

    faask

---
+++ toBinary.java
@@ -0,0 +1,15 @@
+package bridgelabz.utility;
+
+public class toBinary 
+{
+	
+public static void main(String[] args) 
+{
+System.out.println("enter the number");
+int n=utility.getint();
+utility.toBinary(n);
+}
+}
+
+
+
